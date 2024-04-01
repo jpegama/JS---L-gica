7240b80b5091ab02f74e78d5523e94a23779c5b9
João Pedro Gama <111205356+jpegama@users.noreply.github.com>
2024-04-01 10:06:13 -0300

    Delete exercicio6_for directory

--- exercicio6_for/tabuada/tabuada.js
+++
@@ -1,8 +0,0 @@
-const numero = parseFloat(prompt("Digite o número a ser multiplicado: "));
-let resultados = "";
-
-for (let multiplicador = 1; multiplicador <= 20; multiplicador++) {
-  resultados +=
-    numero + " x " + multiplicador + " = " + numero * multiplicador + "\n";
-}
-alert("Resultado da Tabuada de " + numero + ":\n\n" + resultados);
